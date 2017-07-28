9b6847970b9a82b364e1aed6923e68b3578deedb
jocreed <jocreed@cisco.com>
2017-07-28 11:55:53 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:alpine
+
+ENV TMPDIR /tmp
+
+RUN apk add -Uuv --no-cache python3 \
+    && apk upgrade -v --available --no-cache \
+    && apk add ca-certificates && pip3 install --no-cache-dir --upgrade pip setuptools wheel \
+    && pip3 install requests certifi && npm install node-rest-client
+
+ADD https://github.com/alexellis/faas/releases/download/0.5.6-alpha/fwatchdog /usr/bin
+
+RUN chmod +x /usr/bin/fwatchdog
+
+WORKDIR /root/
+
+COPY time2nodejs.py .
+
+ENV fprocess="python3 time2nodejs.py"
+
+HEALTHCHECK --interval=1s CMD [ -e /tmp/.lock ] || exit 1
+
+CMD ["fwatchdog"]
---
+++ time2nodejs.py
@@ -0,0 +1,37 @@
+import sys
+import os
+from io import StringIO
+import contextlib
+
+
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
+
+
+def get_stdin():
+    buf = ""
+    for line in sys.stdin:
+        buf = buf + line
+    return buf
+
+
+if __name__ == "__main__":
+    st = get_stdin()
+    # print(st)
+    with stdoutIO() as s:
+        try:
+            tmpfold = os.environ["TMPDIR"]
+            tmpfile = "%s%s" % (tmpfold, "script.js")
+            f = open(tmpfile, 'w')
+            print(st, file=f)
+            f.close()
+            os.execlp("npm", "", tmpfile)
+        except BaseException as e:
+            print(e)
+    print(s.getvalue())
